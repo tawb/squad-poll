fc80fe0a1c663f17ff6d1a3e5669d363937f228b
Tawba Abdallah <tawba222@proton.me>
2026-08-13 16:14:31 +0300

    Add createFreeformPoll to PollService, wiring Builder with the registry

--- src/facade/PollService.java
+++ src/facade/PollService.java
@@ -6,11 +6,21 @@ import uifactory.PollUIFactory;
 import domain.Poll;
 import factory.PollFactory;
 import java.util.List;
+import builder.PollBuilder;
 public class PollService {
     private final PollRegistry registry = PollRegistry.getInstance();
     private final PollUIFactory uiFactory = new ConsolePollUIFactory();
     public Poll createPoll(PollFactory factory, String question, List<String> options) {
         Poll poll = factory.createPoll(question, options);
+        registry.register(poll);
+        return poll;
+    }
+    public Poll createFreeformPoll(String question, String... options) { //the string ...:It lets the caller pass in any number of separate String arguments
+        PollBuilder builder = new PollBuilder().question(question);
+        for (String option : options) {
+            builder.addOption(option);
+        }
+        Poll poll = builder.build();
         registry.register(poll);
         return poll;
     }
