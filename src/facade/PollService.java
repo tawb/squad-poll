7d7bd40e8e9af84477805669943390ddc061873a
Tawba Abdallah <tawba222@proton.me>
2026-08-13 16:12:20 +0300

    Add createPoll to PollService, wiring Factory Method with the registry

--- src/facade/PollService.java
+++ src/facade/PollService.java
@@ -3,7 +3,15 @@ package facade;
 import registry.PollRegistry;
 import uifactory.ConsolePollUIFactory;
 import uifactory.PollUIFactory;
+import domain.Poll;
+import factory.PollFactory;
+import java.util.List;
 public class PollService {
     private final PollRegistry registry = PollRegistry.getInstance();
     private final PollUIFactory uiFactory = new ConsolePollUIFactory();
+    public Poll createPoll(PollFactory factory, String question, List<String> options) {
+        Poll poll = factory.createPoll(question, options);
+        registry.register(poll);
+        return poll;
+    }
 }
